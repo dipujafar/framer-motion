35686940fac7651689463c587b3165541ab4f079
unknown <dipujafarmr@gmail.com>
2024-09-30 17:48:52 +0600

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import image from "@/assets/Screenshot 2024-09-26 210904.png";
+import List from "@/components/list";
 
 const AboutPage = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <Image src={image} alt="image"></Image>
+      {/* <Image src={image} alt="image"></Image> */}
+      <List></List>
     </div>
   );
 };
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,47 @@
 "use client";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.3 },
+  },
+};
+
+const itemVariant = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5 },
+  },
+};
 
 const List = () => {
-  return <div></div>;
+  return (
+    <motion.ul
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+      className="space-y-2"
+    >
+      {Array.from({ length: 5 }).map((data, idx) => (
+        <motion.li
+          key={idx}
+          variants={itemVariant}
+          className="p-2 bg-gray-400 w-full"
+        >
+          Item {idx + 1}
+        </motion.li>
+      ))}
+    </motion.ul>
+  );
 };
 
 export default List;
