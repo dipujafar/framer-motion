8a95dc70131dec7930c9023a530ff468733f95a6
unknown <dipujafarmr@gmail.com>
2024-09-30 12:52:11 +0600

    learn more animation

--- src/Provider/Provider.tsx
+++ src/Provider/Provider.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const pathName = usePathname();
   return (
     <AnimatePresence>
       <motion.div
+        key={pathName}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
       >
         {children}
       </motion.div>
---
+++ src/components/List.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const List = () => {
+  return <div></div>;
+};
+
+export default List;
